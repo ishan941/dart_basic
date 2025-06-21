d0b11fc235205562ecf70a19f85b951fb98818f3
Ishan Shrestha <iahan.esan18@gmail.com>
2025-06-21 09:12:52 +0545

    banking

--- weather/domain/repositories/weather_repository.dart
+++
@@ -1,5 +0,0 @@
-import 'package:ai_agriculture_app/features/weather/domain/entities/weather_forecast.dart';
-
-abstract class WeatherRepository {
-  Future<WeatherForecast> getWeatherForecast(double lat, double lon);
-}
